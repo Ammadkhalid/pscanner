01d3b412633b30c4e48cc586ed12849529a15ee3
AmmadKhalid <ammadkhalid12@gmail.com>
2018-04-19 01:40:26 +0500

    launched

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pscanner',
+    version = '0.1',
+    author = 'Ammad Khalid',
+    author_email = 'ammadkhalid12@gmail.com',
+    packages = find_packages(),
+    install_requires = ['requests'],
+    python_requires = '>= 3',
+    entry_points = {
+        'console_scripts': ['pscanner = pscanner.cli.__main__:main'],
+    }
+)
